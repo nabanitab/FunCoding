4accc85798c7428bcb41d99bd32b6bde58f2a7fd
nabanitab <nabanitabapari@ca-one.com>
2023-07-20 00:06:57 +0530

    first commit

--- src/main/java/Exception/CustomizedException1.java
+++ src/main/java/Exception/CustomizedException1.java
@@ -4,6 +4,7 @@ public class CustomizedException1 {
 
     public static void main(String[] args) {
         System.out.println("nabanita");
+
         try {
             System.out.println(10 / 0);
         } catch (ArithmeticException e) {
